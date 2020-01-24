36cfaecb033d66b45aef4d077a43d86bc5f9bb41
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-01-24 11:20:23 +0900

    Touch Event,Gesture Event

--- app/src/main/java/com/gardenlab/sampleevent/MainActivity.java
+++ app/src/main/java/com/gardenlab/sampleevent/MainActivity.java
@@ -2,12 +2,96 @@ package com.gardenlab.sampleevent;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private View view1;
+    private View view2;
+    GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        view1 = findViewById(R.id.view1);
+        view2 = findViewById(R.id.view2);
+
+        view1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+
+                float curX = event.getX();
+                float curY = event.getY();
+
+                if(action == MotionEvent.ACTION_DOWN) {
+                    println("손가락 눌림 : "+curX + ", "+ curY);
+                } else if (action == MotionEvent.ACTION_MOVE) {
+                    println("손가락 움직임 : "+ curX + ", " + curY);
+                } else if (action == MotionEvent.ACTION_UP) {
+                    println("손가락 뗌 : "+ curX + ", "+curY);
+                }
+
+                return true;
+            }
+        });
+
+        gestureDetector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                println("onDown() called");
+                return true;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {
+                println("onShowPress() called");
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                println("onSingleTapUp() called");
+                return true;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                println("onScroll() called : "+ distanceX + ", " + distanceY);
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+                println("onLongPress() called");
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                println("onFling() called : " + velocityX + ", "+ velocityY);
+                return true;
+            }
+        });
+
+        view2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                gestureDetector.onTouchEvent(event);
+                return true;
+            }
+        });
     }
+
+    public void println(String data) {
+        textView.append(data+"\n");
+    }
+
+
+
 }
